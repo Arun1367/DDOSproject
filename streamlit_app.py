398a90765658545d8bcf01553423bf5cce337116
ABHISHEKSASA <146556005+ABHISHEKSASA@users.noreply.github.com>
2025-03-08 15:20:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,6 @@ if uploaded_file is not None:
 
                     prediction = (cnn_model.predict(input_reshaped) > 0.5).astype("int32")
                     st.error("🚨 DDoS Attack Detected!") if prediction[0] == 1 else st.success("✅ Benign Traffic Detected!")
-                except Exception as e:
-                        st.error(f"Error reading file: {e}")
-        
 
+    except Exception as e:
+        st.error(f"Error reading file: {e}")
